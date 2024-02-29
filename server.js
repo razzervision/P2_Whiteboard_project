fb6a80066e526ebc991c0ea29add3c12c127ac9b
Mikkel Albertsen <mikkel.skagen@gmail.com>
2024-02-29 14:50:39 +0100

    refactor: move files to public folder.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
+const path = require('path');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/views/index.html');
 });
+
+app.use(express.static('front-end'));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
