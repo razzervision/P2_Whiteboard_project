4c5f6f0fcc4ba5fbd5b6e7322a75e27e10742278
Mikkel Albertsen <mikkel.skagen@gmail.com>
2024-03-25 13:28:02 +0100

    Refactor timer.js and index.ejs

--- public/javascripts/timer.js
+++ public/javascripts/timer.js
@@ -1,30 +1,28 @@
 
 
-
 // Get the modal
-var modal = document.getElementById("myModalforPauses");
+const modal = document.getElementById("myModalforPauses");
 
 // Get the button that opens the modal
-var btn = document.getElementById("pauses");
+const btn = document.getElementById("pauses");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close");
+const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
-}
+    modal.style.display = "block";
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
-}
+    modal.style.display = "none";
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
 
