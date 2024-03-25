833dcfd2bcf18a60160f3c2e5a60df0191e6fe5f
Mikkel Albertsen <mikkel.skagen@gmail.com>
2024-03-25 13:01:33 +0100

    Update Socket.io connection and add socket.io-client dependency

--- routes/togetherPaint.js
+++
@@ -1,9 +0,0 @@
-// const express = require("express");
-// const router = express.Router();
-
-// /* GET togetherPaint page. */
-// router.get("/:room", function(req, res, next) {
-//     res.render("togetherPaint", { room: req.params.room, title: "Together Paint"});
-// });
-
-// module.exports = router;
