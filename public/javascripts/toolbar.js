649490db08194c2b7f749f9ac0581a172089950b
jonvi <jonvincent@live.dk>
2024-03-25 15:18:47 +0100

    Opdateret toolbar

--- public/javascripts/toolbar.js
+++ public/javascripts/toolbar.js
@@ -1,28 +1,42 @@
-const toolbarItems = document.querySelectorAll(".toolbar li");
+const paintButton = document.getElementById("paint_button");
+const quizButton = document.getElementById("quiz_button");
+const calcButton = document.getElementById("calc_button");
+const codeButton = document.getElementById("code_button");
 
-toolbarItems.forEach(item => {
-    item.addEventListener("click", () => {
-        const clickedItemId = item.id;
-        switch (clickedItemId) {
-        case "paint_button":
-            console.log("Paint button clicked");
-        
-            break;
-        case "code_button":
-            console.log("Code-editor button clicked");
-            
-            break;
-        case "quiz_button":
-            console.log("Quiz button clicked");
-            
-            break;
-        case "calc_button":
-            console.log("Calculator button clicked");
-            
-            break;
-        default:
-            
-            break;
-        }
-    });
+const paintProgram = document.getElementById("paintProgram");
+const codeProgram = document.getElementById("codeProgram");
+const quizProgram = document.getElementById("quizProgram");
+const calcProgram = document.getElementById("calcProgram");
+
+
+paintButton.addEventListener("click", function() {
+    if (paintProgram.style.display === "none") {
+        paintProgram.style.display = "block";
+    } else {
+        paintProgram.style.display = "none";
+    }
+});
+
+codeButton.addEventListener("click", function() {
+    if (codeProgram.style.display === "none") {
+        codeProgram.style.display = "block";
+    } else {
+        codeProgram.style.display = "none";
+    }
+});
+
+quizButton.addEventListener("click", function() {
+    if (quizProgram.style.display === "none") {
+        quizProgram.style.display = "block";
+    } else {
+        quizProgram.style.display = "none";
+    }
+});
+
+calcButton.addEventListener("click", function() {
+    if (calcProgram.style.display === "none") {
+        calcProgram.style.display = "block";
+    } else {
+        calcProgram.style.display = "none";
+    }
 });
