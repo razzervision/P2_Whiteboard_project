b7b44c971b17243dda7a3f58a7c4e8929e521fee
Mikkel Albertsen <mikkel.skagen@gmail.com>
2024-04-15 13:57:06 +0200

    added a sqlite database to the project

---
+++ database/database.js
@@ -0,0 +1,10 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("whiteboardDB", "root", "root", {
+    host: "./database/database.sqlite",
+    dialect: "sqlite",
+    logging: false
+});
+
+
+module.exports = sequelize;
