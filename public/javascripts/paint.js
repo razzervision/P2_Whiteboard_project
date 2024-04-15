a76d917bdee007c56b7664075ad7cf5eb6e7a045
lukasgjorup <SA86FJ@student.aau.dk>
2024-04-15 12:21:12 +0200

    123

--- public/javascripts/paint.js
+++ public/javascripts/paint.js
@@ -35,10 +35,8 @@ function uploadePicture(event){
     img.src = URL.createObjectURL(this.files[0]);
   };
   function draw() {
-    var canvas = document.getElementById('canvas');
     canvas.width = this.width;
     canvas.height = this.height;
-    var ctx = canvas.getContext('2d');
     ctx.drawImage(this, 0,0);
   }
   function failed() {
