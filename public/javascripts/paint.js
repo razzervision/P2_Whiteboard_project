a892231a2cf595d884404c61583b80b436c4c290
lukasgjorup <SA86FJ@student.aau.dk>
2024-04-15 12:20:17 +0200

    123

--- public/javascripts/paint.js
+++ public/javascripts/paint.js
@@ -29,24 +29,21 @@ uploadInput.addEventListener("change", uploadePicture)
 
 
 function uploadePicture(event){
-    const file = event.target.files[0];
-    if (!file) return;
-
-    const reader = new FileReader();//reads the url of the file
-    reader.readAsDataURL(file);
-    reader.onload = function(event) {
-        const img = new Image(); //takes the event and says its a image.
-        img.src = event.target.result;//here you set where the reader found file to be the img.src
-        img.onload = function() {
-
-            context.drawImage(img, 0, 0, canvas.width, canvas.height);
-        };
-    };
-    //The readAsDataURL() method of the FileReader object reads the contents of the specified file 
-    //(passed as an argument) and returns the data as a data URL. This line reads the content of the file (file) selected by the
-    // user and converts it into a data URL. Once the file is read, the onload event of the 
-    //FileReader object will be triggered, and you can access the data URL containing the file's 
-    //contents using event.target.result.
+    var img = new Image();
+    img.onload = draw;
+    img.onerror = failed;
+    img.src = URL.createObjectURL(this.files[0]);
+  };
+  function draw() {
+    var canvas = document.getElementById('canvas');
+    canvas.width = this.width;
+    canvas.height = this.height;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(this, 0,0);
+  }
+  function failed() {
+    console.error("The provided file couldn't be loaded as an Image media");
+  
 };
 
 
