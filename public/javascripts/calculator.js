36c5b1afcb3800e4baa65e5c9b0f11ceec33136c
emcharif <emilcharif@gmail.com>
2024-04-22 18:27:47 +0200

    elsker mads

--- public/javascripts/calculator.js
+++ public/javascripts/calculator.js
@@ -1,6 +1,7 @@
+
+
 function addToResult(value) {
     document.getElementById('display').value += value; //updates for every digit clicked to 'display', which is textarea
-    document.getElementById('classHistory').value += value; //updates for every digit clicked to 'classHistory', which is textarea
 }
 
 function clearDisplay() {
@@ -9,9 +10,21 @@ function clearDisplay() {
 
 function calculate() {
     let expression = document.getElementById('display').value;
+
+    for (let i = 0; i < expression.length; i++) {
+        if (expression[i] == "!"){
+            factorial(expression);
+        }
+        if (expression[i] == "^"){
+
+        }
+    }
+
     let result = eval(expression); //eval function calculates with js calculator. ie. Math.pow(5,1) & 5*2
     document.getElementById('display').value = result; //updates display with result
-    document.getElementById('classHistory').value += '\n= ' + result + '\n';  //creates newline after calculation
+    document.getElementById('classHistory').value += expression + '\n= ' + result + '\n';  //creates newline after calculation
+    document.getElementById('classHistory').value += value
+
 }
 
 function clearHistory() {
@@ -19,8 +32,6 @@ function clearHistory() {
 }
 
 function negative(value) {
-
-   
 
     if (document.getElementById('display').value[0] != "-") {
         document.getElementById('display').value = "-" + document.getElementById('display').value;
@@ -30,6 +41,26 @@ function negative(value) {
     }
 
 }
+
+function power(expression) {
+    let num = parseInt(expression);
+    let result = 1;
+
+}
+
+
+function factorial(expression) {
+    let num = parseInt(expression); // Convert expression to integer
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+        result *= i;
+    }
+    document.getElementById('display').value = result; // Update display with factorial result
+    document.getElementById('classHistory').value += expression + '\n= ' + result + '\n';  //creates newline after calculation
+    document.getElementById('classHistory').value += value
+}
+
+
 
 
 /*
