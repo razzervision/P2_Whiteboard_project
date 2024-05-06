9bbc4dfb6076447fb1fa13c33601c17e88e8264b
Mikkel Albertsen <mikkel.skagen@gmail.com>
2024-05-06 14:42:56 +0200

    fixed socket.js

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,14 +1,15 @@
-const checkbox = document.getElementById("connectSocket");
-const serverurl = document.location.origin;
-const socket = io(serverurl, { autoConnect: false });
+(function() {
+    const checkbox = document.getElementById("connectSocket");
+    const serverurl = document.location.origin;
+    const socket = io(serverurl, { autoConnect: false });
 
-checkbox.addEventListener("change", () => {
-    if (checkbox.checked) {
-        socket.connect();
-        console.log("connected");
-    } else {
-        socket.disconnect();
-        console.log("disconnected");
-    }
-});
-
+    checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
+            socket.connect();
+            console.log("connected");
+        } else {
+            socket.disconnect();
+            console.log("disconnected");
+        }
+    });
+})();
