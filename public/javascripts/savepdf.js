9b0fac78bf84483ac03606669c99d6621d790828
jonvi <jonvincent@live.dk>
2024-05-08 11:22:03 +0200

    opdateret save pdf

--- public/javascripts/savepdf.js
+++ public/javascripts/savepdf.js
@@ -1,14 +1,16 @@
 function convertHTMLtoPDF() {
     const { jsPDF } = window.jspdf;
-
     const doc = new jsPDF("l", "mm", [1500, 1400]);
     const pdfjs = document.getElementById("webPage");
 
     doc.html(pdfjs, {
-        callback: function(doc) {
-            doc.save("newpdf.pdf");
-        },
         x: 12,
-        y: 12
-    });			 
-}	 
+        y: 12 
+    })
+        .then(() => {
+            doc.save("newpdf.pdf");
+        })
+        .catch(error => {
+            console.error("Error generating PDF:", error);
+        });
+} 
