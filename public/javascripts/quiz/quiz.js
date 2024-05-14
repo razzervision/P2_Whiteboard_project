a21b9991d050ed3e79a549d45cd284bcd812ba5c
mpersson <Markuspersson121@gmail.com>
2024-05-14 09:58:03 +0200

    toolbar

--- public/javascripts/quiz/quiz.js
+++ public/javascripts/quiz/quiz.js
@@ -703,7 +703,7 @@ function groupAnswersByUser(data) {
 }
 
 //---------------------------------------------------------------------------------------tests
-quizUnitTests();
+// quizUnitTests();
 async function quizUnitTests(){
     const pass = [];
     let passCounter = 0;    
