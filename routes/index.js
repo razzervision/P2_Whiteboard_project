85e245f6b7e83d0fb77a65c7dbbe2009410058ca
jonvincent <KQ81LC@student.aau.dk>
2024-05-14 13:56:02 +0200

    Skrevet TODO

--- routes/index.js
+++ routes/index.js
@@ -381,6 +381,7 @@ async function doPause(data){
     let averageTimeLeft = 0;
     const highestData = [data[0], data[1]];
 
+    //TODO Sørg for først at køre loopet efter andet stykke data er kommet med.
     for (let index = data.length - 1; index >= 0; index--) {
         if(data[index].websiteActivity > highestData[0].websiteActivity ){
             highestData[1] = highestData[0];
