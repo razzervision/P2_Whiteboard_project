84bbdbfab7d4f6b579b2b98cee6106f6c840c3f3
Mikkel Albertsen <mikkel.skagen@gmail.com>
2024-05-14 12:59:06 +0200

    file cleanup

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 const sequelize = require("../database/database");
-const User = require("../database/user");
 const Quiz = require("../database/database_quiz");
 const Pauses = require("../database/database_pauses.js");
 const paint = require("../database/database_paint.js");
@@ -14,18 +13,6 @@ router.get("/", function(req, res, next) {
     res.render("index", { title: "Mikkel er cool" });
 });
 
-router.post("/users", (req, res) => {
-    User.create(req.body).then((user) => {
-        res.status(201).json(user);
-    }).catch((error) => {
-        console.error(error);
-        res.status(400).send("Error creating user");
-    });
-});
-
-router.get("/:room", function(req, res, next) {
-    res.render("index", { room: req.params.room, title: "Together Paint"});
-});
 //------------------------------------------------------------------------------------- code editor
 router.get("/api/loadLanguages", (req, res) => {
     try {
